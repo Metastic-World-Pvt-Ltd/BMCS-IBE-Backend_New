1e19acb114c5d5a35eb9e05314b7451042df3066
Anuj Sahu <info@metasticwolrd.com>
2023-11-08 12:57:06 +0530

    delete product updated v2

--- controllers/Products/deleteProduct.js
+++ controllers/Products/deleteProduct.js
@@ -5,13 +5,14 @@ const logger = require('../User/logger');
 const DeletedProject = require('../../models/DeletedProject');
 const errorMessages = require('../../response/errorMessages');
 const successMessages = require('../../response/successMessages');
+var CryptoJS = require("crypto-js");
 require('dotenv').config({path:'../../.env'});
 
 module.exports.deleteProduct = async function(req , res){
 try {
     logger.info(successMessages.DELETE_PRODUCT_ACTIVATED)
     //token input
-    const token = req.body.token || req.query.token || req.headers["x-access-token"];
+    var token = req.body.token || req.query.token || req.headers["x-access-token"];
     //product id to delete the product
     const productId = req.params.id || req.body.id || req.query.id || req.headers["id"];
     // console.log("Id", _id);
@@ -20,12 +21,15 @@ try {
         logger.error(errorMessages.TOKEN_NOT_FOUND)
         return res.status(401).json(errorMessages.TOKEN_NOT_FOUND);
     }
-    var decode;
+   
     var userRole;
     try {
-        //decode token signature
-        const secret = process.env.SECRET_KEY;
-         decode = jwt.verify(token , secret);
+         //decode token signature
+         const secret = process.env.SECRET_KEY;
+         // Decrypt
+         var bytes  = CryptoJS.AES.decrypt(token, secret);
+         token = bytes.toString(CryptoJS.enc.Utf8);
+        const decode = jwt.verify(token , secret);
     //check for user role as per token
          userRole = decode.role;
     } catch (error) {
