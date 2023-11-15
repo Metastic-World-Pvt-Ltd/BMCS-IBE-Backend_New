6cd24dc3a6b2fd877db1448f5979a7dfc504b697
Anuj Sahu <info@metasticwolrd.com>
2023-11-15 10:22:28 +0530

    Modified Get KYC API

--- controllers/Admin/getInpKyc.js
+++ controllers/Admin/getInpKyc.js
@@ -9,24 +9,33 @@ try {
     logger.info(successMessages.GET_INP_KYC)
     const status = req.headers['status'];
     if(!status){
-        logger.error(errorMessages.ALL_FIELDS_REQUIRED);
-        return res.status(400).json(errorMessages.ALL_FIELDS_REQUIRED)
-    } 
-
-    try {
-        const data = await Kyc.find({status});
-
-        if(data.length == 0){
-            logger.error(errorMessages.NOT_FOUND)
-            return res.status(404).json(errorMessages.NOT_FOUND)
-        }else{       
-            logger.info(successMessages.DATA_SEND_SUCCESSFULLY) 
-            return res.status(200).json(data)
+        try {
+            const data =  await Kyc.find();
+            if(data.length == 0){
+                return res.status(404).json(errorMessages.NOT_FOUND)
+            }else{
+                return res.status(200).json(data);
+            }
+        } catch (error) {
+            logger.error(error)
+            return res.status(502).json(errorMessages.BAD_GATEWAY)
         }
-    } catch (error) {
-        logger.error(error)
-        return res.status(502).json(errorMessages.BAD_GATEWAY)
-    }
+    }else{
+        try {
+            const data = await Kyc.find({status});
+    
+            if(data.length == 0){
+                logger.error(errorMessages.NOT_FOUND)
+                return res.status(404).json(errorMessages.NOT_FOUND)
+            }else{       
+                logger.info(successMessages.DATA_SEND_SUCCESSFULLY) 
+                return res.status(200).json(data)
+            }
+        } catch (error) {
+            logger.error(error)
+            return res.status(502).json(errorMessages.BAD_GATEWAY)
+        }
+    } 
     
 } catch (error) {
     logger.error(error);
