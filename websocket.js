0b6c898774a327c77402a53bb33c5e61d4021294
anujsahumeta <anuj@metasticworld.com>
2023-12-08 16:00:44 +0530

    web socket check v3

--- websocket.js
+++ websocket.js
@@ -42,7 +42,7 @@ const {status} = req.body;
 });
 
 // Start the server
-const PORT =  5000;
+const PORT =  6000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
