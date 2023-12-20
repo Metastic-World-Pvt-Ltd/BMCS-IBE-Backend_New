60a42f3daadb9eee89208bf4a32e1f89331d2d00
anujsahumeta <anuj@metasticworld.com>
2023-12-20 12:20:41 +0530

    Admin ID v3

--- controllers/Admin/adminLogin.js
+++ controllers/Admin/adminLogin.js
@@ -63,7 +63,7 @@ try {
     function encToken(token){
         const secret = process.env.SECRET_KEY;
         var token =  CryptoJS.AES.encrypt(token, secret).toString();
-        console.log(token);
+      
         return token;
     }
 } catch (error) {
