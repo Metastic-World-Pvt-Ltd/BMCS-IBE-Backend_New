21119f7d6470f524b6b7751373978c308e6b5073
anujsahumeta <anuj@metasticworld.com>
2023-12-22 11:44:09 +0530

    getkyc with pagination done

--- controllers/Admin/getInpKyc.js
+++ controllers/Admin/getInpKyc.js
@@ -8,15 +8,27 @@ try {
     logger.info(successMessages.START)
     logger.info(successMessages.GET_INP_KYC)
     const status = req.headers['status'];
+    const page = parseInt(req.query.page) || 1;
+    const limit = 8;
+
     if(!status){
         try {
-            const data =  await Kyc.find();
+            const count =  await Kyc.countDocuments();
+
+            const data =  await Kyc.find()
+            .skip((page -1)*limit)
+            .limit(limit)
+
             if(data.length == 0){
                 logger.error(errorMessages.NOT_FOUND)
                 return res.status(404).json(errorMessages.NOT_FOUND)
             }else{
                 logger.info(successMessages.DATA_SEND_SUCCESSFULLY)
-                return res.status(200).json(data);
+                return res.status(200).json({
+                    page,
+                    totalPages: Math.ceil(count / limit),
+                    'Kyc':data
+                });
             }
         } catch (error) {
             logger.error(error)
@@ -24,14 +36,23 @@ try {
         }
     }else{
         try {
-            const data = await Kyc.find({status});
+            const countData = await Kyc.find({status});
+            const count = countData.length;
+
+            const data = await Kyc.find({status})
+            .skip((page -1)*limit)
+            .limit(limit)
     
             if(data.length == 0){
                 logger.error(errorMessages.NOT_FOUND)
                 return res.status(404).json(errorMessages.NOT_FOUND)
             }else{       
                 logger.info(successMessages.DATA_SEND_SUCCESSFULLY) 
-                return res.status(200).json(data)
+                return res.status(200).json({
+                    page,
+                    totalPages: Math.ceil(count / limit),
+                    'Kyc':data
+                })
             }
         } catch (error) {
             logger.error(error)
