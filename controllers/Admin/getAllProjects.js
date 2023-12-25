1a73421d15997c5359766adc5f5ede29b4df902c
anujsahumeta <anuj@metasticworld.com>
2023-12-25 11:15:08 +0530

    updatd projects

--- controllers/Admin/getAllProjects.js
+++ controllers/Admin/getAllProjects.js
@@ -52,7 +52,7 @@ module.exports.getAllProjects = async function(req, res){
                        return res.status(200).json({
                         page,
                         totalPages: Math.ceil(count / limit),
-                        projectData
+                        'data':projectData
                        });
                    }
                } catch (error) {
